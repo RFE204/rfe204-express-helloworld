dba68faf4dac17da987932cab91bb9471e78753b
Sadig Muradov <sadig@ymail.com>
2024-08-14 18:06:23 +0400

    chore: Add UUID library for generating unique IDs

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const fs = require('fs')
+const { v4: uuidv4 } = require('uuid');
 
 //  create 
 const PORT = process.env.PORT || 3000
@@ -21,34 +22,49 @@ app.get('/users', function (req, res) {
 })
 app.post('/users', function (req, res) {
     const new_user = req.body
+    new_user.id = uuidv4(); // generate a new id
     db.users.push(new_user)
     fs.writeFileSync(DB_PATH, JSON.stringify(db))
     res.status(201).json(new_user)
 })
 app.get('/users/:userID', function (req, res) {
 
-    const {userID} = req.params
+    const { userID } = req.params
     const user = db.users.find(u => `${u.id}` === userID)
     res.json(user)
 })
-app.put('/users/:userID', function (req, res) {
 
-    const {userID} = req.params
-    const user = db.users.find(u => `${u.id}` === userID)
-    res.json(user)
-})
-app.patch('/users/:userID', function (req, res) {
 
-    const {userID} = req.params
-    const user = db.users.find(u => `${u.id}` === userID)
-    res.json(user)
-})
+app.put('/users/:userID', updateUser);
+app.patch('/users/:userID', updateUser);
+function updateUser(req, res) {
+    const { userID } = req.params;
+    const userIndex = db.users.findIndex(u => `${u.id}` === userID);
+    if (userIndex !== -1) {
+        // update user except id
+        db.users[userIndex] = {
+            ...db.users[userIndex],
+            ...req.body,
+            id: db.users[userIndex].id
+        };
+        fs.writeFileSync(DB_PATH, JSON.stringify(db));
+        res.json(db.users[userIndex]);
+    } else {
+        res.status(404).json({ error: 'User not found' });
+    }
+}
 app.delete('/users/:userID', function (req, res) {
+    const { userID } = req.params;
+    const userIndex = db.users.findIndex(u => `${u.id}` === userID);
 
-    const {userID} = req.params
-    const user = db.users.find(u => `${u.id}` === userID)
-    res.json(user)
-})
+    if (userIndex !== -1) {
+        const deletedUser = db.users.splice(userIndex, 1);
+        fs.writeFileSync(DB_PATH, JSON.stringify(db));
+        res.json({ message: 'User deleted successfully', user: deletedUser });
+    } else {
+        res.status(404).json({ message: 'User not found' });
+    }
+});
 
 
 
